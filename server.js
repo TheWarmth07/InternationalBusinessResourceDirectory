d1a55ab3bd02850e2452a08cf9aa4d08b87729bd
Mauro <uesat1@yahoo.com>
2019-10-16 11:49:33 -0400

    adding port

--- server.js
+++ server.js
@@ -115,6 +115,6 @@ app.put('/image', (req, res) => {
     .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000')
+app.listen(process.env.PORT, () => {
+    console.log(`App is running on port 3000 ${process.env.PORT}`)
 })
